1bd1b8176c3774c664b8362f011f0c5e7a573237
jdmaury <jdmaury@uninorte.edu.co>
2015-05-10 17:26:38 -0500

    Username in profile url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'users#index'
-
+  get 'users/:id' => 'users#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
